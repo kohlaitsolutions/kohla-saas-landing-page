88508fac903816d047e360e7a721d3d0e36f2f86
Subi Kikabi <suubiphillip@icloud.com>
2025-04-21 10:36:55 -0700

    Update siteDetails.ts

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,5 +1,5 @@
 export const siteDetails = {
-    siteName: 'Finwise',
+    siteName: 'kohlawise',
     siteUrl: 'https://finwise-omega.vercel.app/',
     metadata: {
         title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
