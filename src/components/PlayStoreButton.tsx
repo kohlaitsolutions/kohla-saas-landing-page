0a8c5af8cea0d4e85d3741509b283661b75acbc9
Subi Kikabi <suubiphillip@icloud.com>
2025-04-21 11:03:32 -0700

    Update PlayStoreButton.tsx

--- src/components/PlayStoreButton.tsx
+++ src/components/PlayStoreButton.tsx
@@ -5,7 +5,7 @@ import { ctaDetails } from '@/data/cta'
 
 const PlayStoreButton = ({ dark }: { dark?: boolean }) => {
     return (
-        <a href={ctaDetails.googlePlayUrl}>
+        <a href={https://play.google.com/store/games?hl=es_419&pli=1}>
             <button
                 type="button"
                 className={clsx("flex items-center justify-center min-w-[205px] mt-3 px-6 h-14 rounded-full w-full sm:w-fit", { "text-white bg-foreground": dark, "text-foreground bg-white": !dark })}
