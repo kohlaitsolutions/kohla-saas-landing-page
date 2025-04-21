88687487a2328a58cbac6184a3cf62fed5c0b643
Subi Kikabi <suubiphillip@icloud.com>
2025-04-21 10:38:14 -0700

    Update siteDetails.ts

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -2,7 +2,7 @@ export const siteDetails = {
     siteName: 'kohlawise',
     siteUrl: 'https://finwise-omega.vercel.app/',
     metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
+        title: 'Kohlawise financial brokers',
         description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
     },
     language: 'en-us',
