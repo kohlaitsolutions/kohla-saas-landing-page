138680602230d8a357b15f0901da0ebdd273b60f
Subi Kikabi <suubiphillip@icloud.com>
2025-04-21 11:05:30 -0700

    Update cta.ts

--- src/data/cta.ts
+++ src/data/cta.ts
@@ -2,5 +2,5 @@ export const ctaDetails = {
     heading: 'Join Over 1 Million Users To Transform Your Finances',
     subheading: 'Your journey to financial freedom starts here. Download Finwise today and take the first step towards a brighter financial future!',
     appStoreUrl: '#',
-    googlePlayUrl: '#'
+    googlePlayUrl: 'https://play.google.com/store/games?hl=es_419&pli=1'
 }
